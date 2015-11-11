78037ae355ef5ee9718b7f081ecbf76f61cd2afb
Robert Brünig <rbr@iqusystems.com>
2015-11-11 12:14:11 +0100

    added empty enter and leave simlulation area state classes and added get path to IState

---
+++ tum_model/EnterSimulationAreaState.java
@@ -0,0 +1,11 @@
+package tum_model;
+
+/**
+ * Created by rober on 11-Nov-15.
+ */
+
+
+public class EnterSimulationAreaState implements IState {
+
+
+}
--- tum_model/IState.java
+++ tum_model/IState.java
@@ -1,10 +1,13 @@
 package tum_model;
 
+import movement.Path;
+import movement.TumCharacter;
+
 /**
  * Created by rober on 11-Nov-15.
  */
 public interface IState {
 
-    
+    Path getPath(TumCharacter node);
 
 }
