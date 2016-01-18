10871d7218d071801c4cd64eae9f9f25ed9128fd
Robert Brünig <rbr@iqusystems.com>
2016-01-18 13:02:41 +0100

    implemented WebPageDb

---
+++ tum_model/WebPage.java
@@ -0,0 +1,7 @@
+package tum_model;
+
+
+public class WebPage {
+    public int size;
+    public int id;
+}
---
+++ tum_model/WebPageDb.java
@@ -0,0 +1,33 @@
+package tum_model;
+
+import tum_model.WebPage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class WebPageDb {
+
+    WebPageDb(int pageCount, int minSize, int maxSize) {
+        this.zipf = new ZipfGenerator(pageCount, 1);
+
+        webPages = new ArrayList<>(pageCount);
+        for(int i = 0; i < webPages.size(); ++i) {
+            WebPage newPage = new WebPage();
+            newPage.id = i;
+            newPage.size = ThreadLocalRandom.current().nextInt(minSize, maxSize);
+            webPages.set(i, newPage);
+        }
+    }
+
+
+    WebPage getRandomPage()
+    {
+        return webPages.get(zipf.next());
+    }
+
+
+    private List<WebPage> webPages;
+    private ZipfGenerator zipf;
+
+}
